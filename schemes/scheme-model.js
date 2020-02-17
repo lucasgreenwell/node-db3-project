5f7ebb2c91ecd8387a705223f9b7f185cd683540
Lucas Greenwell <lucasgreenwell15@gmail.com>
2020-02-17 16:36:07 -0500

    mvp

---
+++ schemes/scheme-model.js
@@ -0,0 +1,31 @@
+const db = require('../data/db-config');
+// console.log(db('schemes').select('*'));
+
+function find(){
+    return db('schemes').select('*')
+}
+
+function findById(id){
+    return db('schemes').select('*').where({id: id})
+}
+
+function findSteps(id){
+    return db('steps').select('*').where({scheme_Id: id})
+}
+
+function add(scheme){
+   return db('schemes').insert(scheme)
+}
+
+function update(changes, id){
+     return db('schemes').select('*').where({id: id}).update({...changes})
+}
+
+function remove(id){
+    return db('schemes').select('*').where({id: id}).del()
+
+}
+
+module.exports = {
+    find, findById, findSteps, add, update, remove
+}
